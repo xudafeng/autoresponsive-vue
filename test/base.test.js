a1dfbf0594415fc410ca0bc83c23a4542926dfab
xudafeng <xudafeng@126.com>
2017-11-27 19:50:09 +0800

    add nyc

---
+++ test/base.test.js
@@ -0,0 +1,5 @@
+describe('test', function() {
+  it('test1', function() {
+    
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   externals: [
     {
+      vue: 'window.Vue'
     }
   ],
   module: {
